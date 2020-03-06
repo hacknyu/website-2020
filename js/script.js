b3171d4ec84e4bb142bb97dae6f73767cc098d9e
Michael Yang <themichaelyang@gmail.com>
2020-03-06 02:31:12 -0500

    add scrolling script

--- js/script.js
+++ js/script.js
@@ -32,8 +32,29 @@ function handleURLChange() {
     }
 }
 
+function handleScroll() {
+    const navLinks = [...document.querySelectorAll(".nav-links a")]
+    const sections = [...document.querySelectorAll(".tab")].sort((el1, el2) => el1.offsetTop - el2.offsetTop)
+
+    let currentSection = sections[0]
+
+    navLinks.map((link) => link.classList.remove("navlink-active-orange"))
+
+    // console.log(window.scrollY)
+    for (let sec of sections) {
+        if (window.scrollY > sec.offsetTop - 400) {
+            currentSection = sec
+        }
+    }
+
+    // console.log(currentSection)
+    navLinks.filter((link) => link.href.split('#')[1] == currentSection.id)[0]
+            .classList.add("navlink-active-orange")
+}
+
 function main() {
     window.onhashchange = handleURLChange
+    window.onscroll = handleScroll
     initMobileNav()
 }
 
